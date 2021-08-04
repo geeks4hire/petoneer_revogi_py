4e2c22256d45b75096c293f1c5aa6d2f33f62264
Ben Horan <benh@geeksforhire.com.au>
2021-08-04 13:19:25 +1000

    Created 'petoneerConst.py' to contain all constants across classes.
    Shared onstants include API server and method paths, fountain countdown timer
    durations,  debug mode etc

---
+++ petoneerConst.py
@@ -0,0 +1,21 @@
+API_URL                             = "https://as.revogi.net/app"
+
+API_LOGIN_PATH                      = "/user/101"
+API_DEVICE_LIST_PATH                = "/user/500"
+
+API_DEVICE_DETAILS_PATH             = "/pww/31101"
+API_DEVICE_SCHEDULE_DETAILS_PATH    = "/pww/31102"
+
+API_DEVICE_SWITCH_PATH              = "/pww/21101"
+API_DEVICE_TIMER_PATH               = "/pww/21102"
+API_DEVICE_LED_PATH                 = "/pww/21104"
+
+API_RESET_CLEAN_PUMP_TIMER          = "/pww/21103"
+API_RESET_FILTER_CHANGE_TIMER       = "/pww/21105"
+API_RESET_WATER_CHANGE_TIMER        = "/pww/21107"
+
+SECONDS_FOUNTAIN_WATER_CHANGE       = 5 * 24 * 60 * 60    #  5 days 
+SECONDS_FOUNTAIN_FILTER_CHANGE      = 30 * 24 * 60 * 60   # 30 days
+SECONDS_FOUNTAIN_CLEAN_PUMP         = 60 * 24 * 60 * 60   # 60 days
+
+Debug                           = 1 
